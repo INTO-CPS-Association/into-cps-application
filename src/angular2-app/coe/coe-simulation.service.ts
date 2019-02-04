f2bc886bbd67efb5f9455cbc62e0e4bd57fb3e33
Casper Thule <casper.thule@eng.au.dk>
2019-02-04 19:33:32 +0100

    Fixed issue related to external graphs

--- src/angular2-app/coe/coe-simulation.service.ts
+++ src/angular2-app/coe/coe-simulation.service.ts
@@ -231,11 +231,21 @@ export class CoeSimulationService {
 
     private downloadResults() {
         this.graph.closeSocket();
+        let markedForDeletionExternalGraphs : DialogHandler[]= [];
         this.externalGraphs.forEach((eg) => {
-            if (eg.win)
+            if (!eg.win.isDestroyed()){
                 //This also causes a redraw event for the external graphs.
                 eg.win.webContents.send("close");
-        })
+            }
+            else{
+                // The window have been destroyed, remove it from external graphs
+                markedForDeletionExternalGraphs.push(eg);
+            }
+        });
+        markedForDeletionExternalGraphs.forEach((eg) => {
+            this.externalGraphs.splice(this.externalGraphs.indexOf(eg, 0),1);
+        });
+
         this.simulationCompletedHandler();
 
         let resultPath = Path.normalize(`${this.resultDir}/outputs.csv`);
