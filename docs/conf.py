a657574451bdc40db3aa17a1bf9837d0639e861a
Christian Legaard <clegaard@outlook.com>
2020-09-03 11:32:24 +0200

    removed auto reference exetnsion

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["recommonmark", "sphinx_rtd_theme", "sphinx.ext.autosectionlabel"]
+extensions = ["recommonmark", "sphinx_rtd_theme"]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
