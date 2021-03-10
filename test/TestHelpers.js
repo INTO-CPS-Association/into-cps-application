54b9c8cfd7ad49428e6bc02a9c1da438376e43c6
Max <max.rose366@gmail.com>
2021-03-10 12:34:26 +0000

    test data can be downloaded

--- test/TestHelpers.js
+++ test/TestHelpers.js
@@ -15,3 +15,33 @@ module.exports.app = function()
         startTimeout: 20000
     })
 }
+
+
+
+module.exports.downloadTestData = async function(dataUrl){
+    const request = require("request");
+    const fs = require("fs");
+    const admZip = require("adm-zip");
+    const path = require("path");
+    const dataPath = path.resolve("test/TestData/");
+    const downloadPath = dataPath + "data.zip";
+
+    console.log("Downloading test data from " + dataUrl);
+
+    return new Promise((resolve, reject) => {
+        request.get(dataUrl)
+            .on("error", error => console.log("Failed to download test data: " + error))
+            .pipe(fs.createWriteStream(downloadPath))
+            .on("finish", () => {
+                console.log("Unzipping test data");
+
+                const zip = new admZip(downloadPath);
+                const folderName = zip.getEntries()[0].entryName;
+
+                zip.extractAllTo(dataPath, true);
+                fs.unlinkSync(downloadPath);
+
+                resolve(dataPath + "/" + folderName);
+            });
+    });
+}
