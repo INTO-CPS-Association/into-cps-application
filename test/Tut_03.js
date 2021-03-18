b562fcb0631fa48046da909aefbb1607c70282ef
Max <max.rose366@gmail.com>
2021-03-18 18:00:54 +0000

    finished generation of mm

--- test/Tut_03.js
+++ test/Tut_03.js
@@ -12,6 +12,11 @@ chai.should();
 chai.use(chaiAsPromised);
 chai.use(chaiWaitFor);
 
+const waitFor = chaiWaitFor.bindWaitFor({
+  timeout: 5000,
+  retryInterval: 100
+});
+
 const app = require("./TestHelpers").app();
 const path = require("path");
 const testDataZipPath = path.resolve("test/TestData/test3_data.zip");
@@ -88,6 +93,8 @@ describe('In Tutorial 3', function () {
         .then(n => n.$$("tr"))
         .then(n => n[0].click())
         .then(() => app.client.$("#w2ui-popup div.w2ui-popup-title"))
+        .then(async n => await n.waitForExist(3000))
+        .then(() => app.client.$("#w2ui-popup div.w2ui-popup-title"))
         .then(n => n.getText())
         .should
         .eventually
@@ -106,24 +113,120 @@ describe('In Tutorial 3', function () {
         .contain("mm-3DRobot");
   });
 
+  it("Open configuration to edit", function () {
+    return app.client.$("#Configuration")
+        .then(n => n.click())
+        .then(() => app.client.$('button.btn.btn-default'))
+        .then(n => n.click())
+        .then(() => app.client.$('button.btn.btn-default'))
+        .then(n => n.getText())
+        .should
+        .eventually
+        .contain("Save");
+  });
 
-  //Step 31
-  // TODO: Need to repeat this 
-  xit('Defining the sensor positions', function () {
-    return this.app.client
-      .waitForVisible('#initialvalsensor1')
-      .$('#initialvalsensor1').click()
+  it("Should contain errors as FMU paths are not defined", function () {
+    return app.client.$$(".alert.alert-warning.alert-big")
+        .then(n => n.length)
+        .should
+        .eventually
+        .be
+        .greaterThan(0);
+  });
 
-      .waitForVisible('#lf_position_y')
-      .$('#lf_position_y').setValue('0.065')
+  it("Should set the Path for c (Controller)", function () {
+    return app.client.$("#fmu0 file-browser input")
+        .then(n => n.setValue("LFRController.fmu"))
+        .then(() => app.client.$("#fmu0 file-browser input"))
+        .then(n => n.getValue())
+        .should
+        .eventually
+        .contain("LFRController.fmu");
+  });
 
-      .waitForVisible('#lf_position_x')
-      .$('#lf_position_x').setValue('0.001').pause(2000)
-      .$('#lf_position_x').getValue()
-      .then(function (text) {
-        assert.equal(text, '0.001')
-      })
-  })
+  it("Should set the Path for b (Body)", function () {
+    return app.client.$("#fmu1 file-browser input")
+        .then(n => n.setValue("Body_Block.fmu"))
+        .then(() => app.client.$("#fmu1 file-browser input"))
+        .then(n => n.getValue())
+        .should
+        .eventually
+        .contain("Body_Block.fmu");
+  });
+
+  it("Should set the Path for 3D (3D Animation)", function () {
+    return app.client.$("#fmu2 file-browser input")
+        .then(n => n.setValue("3DanimationFMU.fmu"))
+        .then(() => app.client.$("#fmu2 file-browser input"))
+        .then(n => n.getValue())
+        .should
+        .eventually
+        .contain("3DanimationFMU.fmu");
+  });
+
+  it("Should set the Path for sensor1", function () {
+    return app.client.$("#fmu3 file-browser input")
+        .then(n => n.setValue("Sensor_Block_01.fmu"))
+        .then(() => app.client.$("#fmu3 file-browser input"))
+        .then(n => n.getValue())
+        .should
+        .eventually
+        .contain("Sensor_Block_01.fmu");
+  });
+
+  it("Should set the Path for sensor2", function () {
+    return app.client.$("#fmu4 file-browser input")
+        .then(n => n.setValue("Sensor_Block_02.fmu"))
+        .then(() => app.client.$("#fmu4 file-browser input"))
+        .then(n => n.getValue())
+        .should
+        .eventually
+        .contain("Sensor_Block_02.fmu");
+  });
+
+  it("Should be able to save configuration", function () {
+    return app.client.$('button.btn.btn-default')
+        .then(n => n.click())
+        .then(() => app.client.$('button.btn.btn-default'))
+        .then(n => n.getText())
+        .should
+        .eventually
+        .contain("Edit");
+  });
+
+  it("Set sensor1 y position", function() {
+    return app.client.$('button.btn.btn-default')
+        .then(n => n.click())
+        .then(() => app.client.$("#initialvalsensor1"))
+        .then(n => n.click())
+        .then(() => app.client.$("#lf_position_y"))
+        .then(n => n.addValue(0.065)) // setValue for some reason does not work in this context
+        .then(() => app.client.$("#lf_position_y"))
+        .then(n => n.getValue())
+        .should
+        .eventually
+        .equal("00.065");
+  });
+
+  it("Set sensor1 x position", function() {
+    return () => app.client.$("#lf_position_x")
+        .then(n => n.addValue(0.001)) // setValue for some reason does not work in this context
+        .then(() => app.client.$("#lf_position_x"))
+        .then(n => n.getValue())
+        .should
+        .eventually
+        .equal("00.001");
+  });
+
+  it("Should save with no errors", function() {
+    return app.client.$('button.btn.btn-default')
+        .then(n => n.click())
+        .then(app.client.$$(".alert.alert-warning.alert-big"))
+        .should
+        .eventually
+        .be
+        .null;
+  });
 
   //Step 34,35
   xit('Right-click on the mm and select Create Co-Simulation Configuration', function () {
