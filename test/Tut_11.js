c4af042711e89f567d8cb85a49d786a60a569ffc
Max <max.rose366@gmail.com>
2021-03-21 17:25:32 +0000

    test 11

--- test/Tut_11.js
+++ test/Tut_11.js
@@ -1,73 +1,70 @@
-const Application = require('spectron').Application
-const expect = require('chai').expect;
-const electronPath = require('electron') // Require Electron from the binaries included in node_modules.
-const path = require('path')
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+// needed so we can use as promised
+chai.should();
+chai.use(chaiAsPromised);
 
+const app = require("./TestHelpers").app();
+const path = require("path");
+const testDataZipPath = path.resolve("test/TestData/test11_data.zip");
+const testDataPath = path.resolve("test/TestData/test11_data");
 
-describe.skip('In Tutorial 11', function () {
+describe('In Tutorial 11', function () {
   this.timeout(120000)
 
+  before(async function () {
 
+    await app.start();
+    await app.client.waitUntilWindowLoaded();
 
-  beforeEach(async function () {
+    await require("./TestHelpers").unZipTestData(testDataZipPath, testDataPath);
+    await app.electron.remote.app.loadProject(testDataPath + "/project/.project.json");
 
-    this.app = new Application({
-      path: electronPath,
-      env: { RUNNING_IN_SPECTRON: '1' },
-      args: [path.join(__dirname, '..')]
-    })
+    return app;
+  });
 
+  after(function () {
+    return require("./TestHelpers").commonShutdownTasks(app, testDataPath);
+  });
 
-    await this.app.start();
-    await this.app.client.waitUntilWindowLoaded();
+  it('Should have tutorial 11 loaded', function () {
+    return app.electron.remote.app.getActiveProject()
+        .should
+        .eventually
+        .equal(testDataPath + "/project/.project.json");
+  });
 
-    return this.app;
+  it("Should have the correct name", function () {
+    return app.electron.remote.app.getIProject()
+        .then(n => { return n
+            .name
+            .should
+            .equal("ipp4cpps-project");
+        });
+  });
 
-  })
+  it("Should open the mm to edit", function () {
+    return app.client.$("#node_ProjectBrowserItem_13")
+        .then(n => n.doubleClick())
+        .then(() => app.client.$("#Configuration"))
+        .then(n => n.click())
+        .then(() => app.client.$('mm-configuration button.btn.btn-default'))
+        .then(n => n.click())
+        .then(() => app.client.$('button.btn.btn-default'))
+        .then(n => n.getText())
+        .should
+        .eventually
+        .contain("Save");
+  });
 
-  afterEach(function () {
-
-    if (this.app && this.app.isRunning()) {
-
-      return this.app.stop()
-        .then(() => {
-          if (this.currentTest.state === 'failed' && this.currentTest.title === 'Should have line follower robot loaded')
-            throw Error("Line follower robot is not loaded!")
-        })
-    }
-  })
-
-  //Step 33
-  //Manually select the line follower robot case study 
-  it('File->Import example project Menu Click', async function () {
-    // TODO remove the multiple hardcoding
-    await app.electron.remote.app.loadProject('/home/hdm/workspaces/into-cps-projects/tutorials/tutorial_11/.project.json');  
-  })
-
-  // This should be done before as soon as we solve the programmatic project load problem
-  it('Should have line follower robot loaded', function () {
-    return this.app.client.waitUntilWindowLoaded()
-      .then(function () {
-        return this.electron.remote.app.getActiveProject().then(r => { expect(r).contain('example-line_follower_robot'); })
-
-      })
-  })
-
-  //Step 35
-  //Manually cancel or select the fmu file
-  it('Add the new FMU instead of the overture FMU', function () {
-    return this.app.client
-      .waitForVisible('#node_ProjectBrowserItem_76')
-      .$('#node_ProjectBrowserItem_76').doubleClick()
-
-      .waitUntilWindowLoaded()
-      .waitForVisible('#Configuration')
-      .$('mm-page').$('#Configuration').click()
-      .waitForVisible('.btn.btn-default')
-      .$('.btn.btn-default').click()
-
-      .waitForVisible('#fmu0')
-      .waitForVisible('#file')
-      .$('#fmu0').$('#file').click().pause(3000);
-  })
-})
+  it("Set HMI FMU", function () {
+    return app.client.$("#fmu0 file-browser input")
+        .then(n => n.setValue("FMU_VDM_HMI.fmu"))
+        .then(() => app.client.$("#fmu0 file-browser input"))
+        .then(n => n.getValue())
+        .should
+        .eventually
+        .equal("FMU_VDM_HMI.fmu");
+  });
+});
