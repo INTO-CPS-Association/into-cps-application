ad460272592678e0e355312bbce57ae6c117dcf5
levisen <jkv@levisen.eu>
2022-05-04 08:27:53 -0700

    Tests only have one try, as other tests are dependent on the prior tests for a testfile.. Changed timout to 15s

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,5 +3,7 @@ import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   // Limit the number of workers on CI, use default locally
   workers: 1,
+  retries: 0,
+  timeout: 15000
 };
 export default config;
