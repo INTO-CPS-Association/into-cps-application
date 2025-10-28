e110a4ddd79f23fa99719b373f1e8c8473f8c2e4
Simone Micalizzi <xkeitas@gmail.com>
2025-10-28 12:16:21 +0100

    chore: New release workflow for automatic draft release with semantic-release

---
+++ release.config.ts
@@ -0,0 +1,30 @@
+import type { Options } from 'semantic-release';
+
+const config: Options = {
+  branches: ['main', { name: 'development', prerelease: true }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        draft: true,
+      },
+    ],
+  ],
+};
+
+export default config;
